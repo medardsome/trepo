0625678fdf37980e3f2e3d95666647ad57513ff5
medardsome <medard.some@gmail.com>
2023-09-17 20:23:08 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Lets Learn C# Programming");
+
+string name = "King Kochhar";
+int age = 23;
+float discunt = 1000.50F;
+double contactNumber = 1234567890;
+char gender = 'M';
+bool isValid = true;
+
+Console.WriteLine("Name : " + name);
+Console.WriteLine("Age : " + age);
+Console.WriteLine("Discount : " + discunt);
+Console.WriteLine("Contact Number : " + contactNumber);
+Console.WriteLine("Gender : " + gender);
+Console.WriteLine("Is Valid : " +  isValid);
+
